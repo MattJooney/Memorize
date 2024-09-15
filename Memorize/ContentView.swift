498251a76398de1e673897983f0e69b9be42b2fb
MattJooney <mkang4153@gmail.com>
2024-09-15 18:20:25 -0400

    adding tap function

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -21,21 +21,27 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = false
+    //could also be written as var isFaceUp = false
+    //because SwiftUI is a strongly typed language, it infers your variables with what you set it equal to
     
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                    base.foregroundColor(.white)
+                    base.strokeBorder(lineWidth: 2)
                 Text("👻").font(.largeTitle)
             }
             else {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.red)
+                    base.foregroundColor(.red)
+                    base.strokeBorder(lineWidth: 2)
             }
+        }
+        .onTapGesture {
+            print("working")
+            isFaceUp = !isFaceUp
+            
         }
     }
 }
